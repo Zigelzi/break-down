98631f62055eaca633c6370733aaa8fc9fff2bf7
Miika Savela <miika.a.savela@gmail.com>
2021-02-27 14:55:48 +0200

    Add door obstacle and door plate trigger functionality

---
+++ Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour
+{
+    [SerializeField] GameObject doorTriggerGameObject;
+    [SerializeField] float openingSpeed = 2f;
+    [SerializeField] bool moveDown = false;
+
+    private float startYPosition;
+    TriggerPlateHandler doorTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startYPosition = transform.position.y;
+        doorTrigger = doorTriggerGameObject.GetComponent<TriggerPlateHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveDoor();
+    }
+
+    private void MoveDoor()
+    {
+        float doorHeight = transform.GetChild(0).localScale.y;
+        float currentYPosition = transform.position.y;
+
+        float positionDifference = startYPosition - currentYPosition;
+        if (positionDifference > doorHeight)
+        {
+            moveDown = false;
+        } else if (positionDifference <= 0)
+        {
+            moveDown = true;
+        }
+        if (moveDown && doorTrigger.platePushed)
+        {
+            transform.Translate(Vector3.down * openingSpeed * Time.deltaTime);
+        } else if (!moveDown && !doorTrigger.platePushed)
+        {
+            transform.Translate(Vector3.up * openingSpeed * Time.deltaTime);
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/TriggerPlateHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlateHandler : MonoBehaviour
+{
+    public bool platePushed = false;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            platePushed = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            platePushed = false;
+        }
+    }
+}
