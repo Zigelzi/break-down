0a32cac9f9a0ecdd29037b123513a20eeedd1ef5
Miika Savela <miika.a.savela@gmail.com>
2021-02-27 13:31:50 +0200

    Add level complete logic and prefab

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    private float levelLoadDelay = 1f;
+
+    // Game State
+    enum State { Alive, Died, Transcending }
+    State gameState = State.Alive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameState != State.Alive)
+        {
+            /* 
+             * Prevent processing any additional collisions when not alive
+             * or when collisions are disabled for debugging
+             */
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            case "Finish":
+                Debug.Log("Level Complete!");
+                break;
+            default:
+                Debug.Log("You hit something else.");
+                break;
+        }
+    }
+}
