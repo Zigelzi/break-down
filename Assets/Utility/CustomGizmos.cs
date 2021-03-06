2bed2ab5408cdaa6dd8cd5461d9b17b620442cdc
Miika Savela <miika.a.savela@gmail.com>
2021-03-06 12:05:36 +0200

    Add halfway drawing custom gizmo

--- Assets/Utility/CustomGizmos.cs
+++ Assets/Utility/CustomGizmos.cs
@@ -5,13 +5,20 @@ public class CustomGizmos : MonoBehaviour
     [SerializeField] Transform source;
     [SerializeField] Transform target;
 
+    private void Start()
+    {
+        source = transform;
+    }
+
     // Start is called before the first frame update
     private void OnDrawGizmosSelected()
     {
         if (source != null && target != null)
         {
             Gizmos.color = Color.magenta;
-            Gizmos.DrawLine(source.position, target.position);
+            Vector3 halfway = source.position - (source.position - target.position) / 2;
+            Debug.Log(halfway);
+            Gizmos.DrawLine(source.position, halfway);
         }
     }
 }
